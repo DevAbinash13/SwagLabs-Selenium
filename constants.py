aff5e3db4ea6144d2080cb56234197a8465750f3
Abinash Sahoo <abinashlucky45@gmail.com>
2024-07-03 14:30:04 +0530

    Add to cart
    Added add to cart and count functionality

--- constants.py
+++ constants.py
@@ -2,3 +2,4 @@ URL= "https://www.saucedemo.com/"
 UID= "standard_user"
 PASS= "secret_sauce"
 WEBSITE_NAME= "Swag Labs"
+LIST_OF_ITEMS=['Sauce Labs Backpack','Sauce Labs Fleece Jacket']
--- test_functional.py
+++ test_functional.py
@@ -36,17 +36,29 @@ class Test_functional_tests():
         #assert that title is products
         base.presence_of_elem((By.XPATH,"//span[@class='title']")).text == "Products"
 
-    def test_login_invalid(self):
+    # def test_login_invalid(self):
+    #     base= self.get_base()
+    #     #logout
+    #     base.elem_clickable((By.XPATH,"//button[@id='react-burger-menu-btn']")).click()
+    #     base.elem_clickable((By.XPATH,"//a[@id='logout_sidebar_link']")).click()
+    #     #login with invalid creds
+    #     # pass username in input
+    #     base.presence_of_elem((By.XPATH, "//input[@id='user-name']")).send_keys("abc")
+    #     # pass password in input
+    #     base.presence_of_elem((By.XPATH, "//input[@id='password']")).send_keys("def")
+    #     # click the login button
+    #     base.elem_clickable((By.XPATH, "//input[@id='login-button']")).click()
+    #     error= base.presence_of_elem((By.XPATH,"//h3[@data-test='error']")).text
+    #     assert error == "Epic sadface: Username and password do not match any user in this service"
+
+    def test_add_to_cart(self):
         base= self.get_base()
-        #logout
-        base.elem_clickable((By.XPATH,"//button[@id='react-burger-menu-btn']")).click()
-        base.elem_clickable((By.XPATH,"//a[@id='logout_sidebar_link']")).click()
-        #login with invalid creds
-        # pass username in input
-        base.presence_of_elem((By.XPATH, "//input[@id='user-name']")).send_keys("abc")
-        # pass password in input
-        base.presence_of_elem((By.XPATH, "//input[@id='password']")).send_keys("def")
-        # click the login button
-        base.elem_clickable((By.XPATH, "//input[@id='login-button']")).click()
-        error= base.presence_of_elem((By.XPATH,"//h3[@data-test='error']")).text
-        assert error == "Epic sadface: Username and password do not match any user in this service"
+        lists = self.driver.find_elements(By.XPATH,"//div[@class='inventory_item_name ']")
+        #action: if matches with the list of names which is present in constants , add to cart
+        for i in constants.LIST_OF_ITEMS:
+            for j in lists:
+                if i == j.text:
+                    base.presence_of_elem((By.XPATH,f"//div[normalize-space()='{i}']/ancestor::div[@class='inventory_item_description']//button")).click()
+        #count the number in the badge and assert with the length of the items list which is present in constants
+        num= base.presence_of_elem((By.XPATH,"//span[@class='shopping_cart_badge']")).text
+        assert num == str(len(constants.LIST_OF_ITEMS))
